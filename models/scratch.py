f674fe4669f0df7391023210fb37c929fae9209f
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-03-03 07:52:43 +0300

    chore(models): remove unused scratch file

--- models/scratch.py
+++
@@ -1,9 +0,0 @@
-# Write two or three lines in a scratch file that manually instantiate a Region, add a case ID, call to_dict(), and print it. 
-# Make sure it looks like what you'd expect to see in a JSON file.
-
-from region import Region
-
-region1 = Region(568," Nyeri", 500000)
-region1.case_list = [237, 458, 273]
-dictionary = region1.to_dict()
-print(dictionary)
